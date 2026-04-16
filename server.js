d8c799384fe7a55fc4c24ff3fe849905cdeb9ff8
mexoOG <jamaine.rahlf0305@gmail.com>
2026-04-16 17:54:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,31 +8,49 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
+app.get("/", (req,res)=>res.redirect("/settings"));
+
 let state = {
-  left: { name: "Kevin", score: 4 },
-  right: { name: "Basti", score: 4 },
-  round: 1
+  left: { name: "Player 1", score: 0 },
+  right: { name: "Player 2", score: 0 },
+  round: 1,
+  bestOf: 5,
+  color: "#00f5d4",
+  lastAction: ""
 };
+
+function clampRound() {
+  if (state.round < 1) state.round = 1;
+}
 
 io.on("connection", (socket) => {
   socket.emit("state", state);
 
-  socket.on("update", (newState) => {
-    state = { ...state, ...newState };
-    io.emit("state", state);
-  });
-
-  socket.on("score", ({ side, delta }) => {
+  socket.on("score", ({side, delta}) => {
     state[side].score += delta;
+    state.lastAction = `${side} ${delta>0?"+":""}${delta}`;
     io.emit("state", state);
   });
 
   socket.on("round", (delta) => {
     state.round += delta;
-    if (state.round < 1) state.round = 1;
+    clampRound();
+    io.emit("state", state);
+  });
+
+  socket.on("update", (data) => {
+    state = { ...state, ...data };
+    io.emit("state", state);
+  });
+
+  socket.on("reset", () => {
+    state.left.score = 0;
+    state.right.score = 0;
+    state.round = 1;
+    state.lastAction = "RESET";
     io.emit("state", state);
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Running on " + PORT));
+server.listen(PORT, ()=>console.log("Running on "+PORT));
