e634ab819ce93eb92b272d63db9f881d3d452045
mexoOG <jamaine.rahlf0305@gmail.com>
2026-04-16 18:24:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const io = new Server(server);
 app.use(express.static("public"));
 
 let state = {
-  left: { name: "Player 1", score: 0 },
-  right: { name: "Player 2", score: 0 },
+  left: { name: "Marlov", score: 0, color: "#ff0044" },
+  right: { name: "Marlon", score: 0, color: "#00aaff" },
   round: 1,
   timer: 0,
   timerRunning: false
 };
 
-// TIMER LOOP
+// TIMER
 setInterval(() => {
   if (state.timerRunning) {
     state.timer++;
@@ -38,24 +38,19 @@ io.on("connection", (socket) => {
     io.emit("state", state);
   });
 
-  socket.on("update", (data) => {
-    state = { ...state, ...data };
-    io.emit("state", state);
-  });
-
-  socket.on("timerStart", () => {
-    state.timerRunning = true;
-  });
-
-  socket.on("timerStop", () => {
-    state.timerRunning = false;
-  });
+  socket.on("timerStart", () => (state.timerRunning = true));
+  socket.on("timerStop", () => (state.timerRunning = false));
 
   socket.on("timerReset", () => {
     state.timer = 0;
     io.emit("state", state);
   });
+
+  socket.on("update", (data) => {
+    state = { ...state, ...data };
+    io.emit("state", state);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Running on " + PORT));
+server.listen(PORT, () => console.log("Running " + PORT));
