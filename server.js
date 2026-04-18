5a84c58de7910b9452c5df49e5e6ed0e50ae5174
mexoOG <jamaine.rahlf0305@gmail.com>
2026-04-18 12:59:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ let state = {
   left: { name: "Marlov", score: 0, nameSize: 32 },
   right: { name: "Marlon", score: 0, nameSize: 32 },
   round: 1,
+  game: "Game 1", // 🔥 NEU
   timer: 0,
   timerRunning: false
 };
@@ -27,36 +28,37 @@ setInterval(() => {
 io.on("connection", (socket) => {
   socket.emit("state", state);
 
-  // SCORE (+ / - FLEX)
   socket.on("scoreAdd", ({ side, amount }) => {
     state[side].score += Number(amount);
     if (state[side].score < 0) state[side].score = 0;
     io.emit("state", state);
   });
 
-  // ROUND + / -
   socket.on("roundChange", (delta) => {
     state.round += Number(delta);
     if (state.round < 1) state.round = 1;
     io.emit("state", state);
   });
 
-  // ROUND SET DIRECT
   socket.on("roundSet", (value) => {
     state.round = Math.max(1, Number(value));
     io.emit("state", state);
   });
 
-  // TIMER
-  socket.on("timerStart", () => (state.timerRunning = true));
-  socket.on("timerStop", () => (state.timerRunning = false));
+  // 🔥 GAME SET
+  socket.on("gameSet", (value) => {
+    state.game = value || "";
+    io.emit("state", state);
+  });
+
+  socket.on("timerStart", () => state.timerRunning = true);
+  socket.on("timerStop", () => state.timerRunning = false);
 
   socket.on("timerReset", () => {
     state.timer = 0;
     io.emit("state", state);
   });
 
-  // SETTINGS UPDATE
   socket.on("update", (data) => {
     state.left = { ...state.left, ...data.left };
     state.right = { ...state.right, ...data.right };
