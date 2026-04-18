6185d128667669f87264e75255f494abb492f764
mexoOG <jamaine.rahlf0305@gmail.com>
2026-04-18 13:07:26 +0200

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,14 +1,10 @@
 const socket = io();
 
-let displayLeft = 0;
-let displayRight = 0;
-
 function animateScore(id, target) {
   let current = Number(document.getElementById(id).textContent);
 
   let interval = setInterval(() => {
     if (current === target) return clearInterval(interval);
-
     current += current < target ? 1 : -1;
     document.getElementById(id).textContent = current;
   }, 20);
@@ -27,14 +23,12 @@ socket.on("state", (s) => {
   document.getElementById("gameText").textContent =
     "Aktuelles Game: " + s.game;
 
-  // TIMER
   let m = Math.floor(s.timer / 60);
   let sec = s.timer % 60;
 
   document.getElementById("timer").textContent =
     String(m).padStart(2,"0")+":"+String(sec).padStart(2,"0");
 
-  // WINNER
   const w = document.getElementById("winnerBox");
 
   if (s.winner) {
@@ -45,25 +39,16 @@ socket.on("state", (s) => {
   }
 });
 
-// CONTROLS
+// controls
 function score(side) {
   const amount = Number(document.getElementById("scoreAmount").value || 1);
   socket.emit("scoreAdd", { side, amount });
 }
 
 function roundChange(d){ socket.emit("roundChange", d); }
-
-function setGame(){
-  socket.emit("gameSet", document.getElementById("gameInput").value);
-}
-
-function winner(side){
-  socket.emit("setWinner", side);
-}
-
-function clearWinner(){
-  socket.emit("clearWinner");
-}
+function setGame(){ socket.emit("gameSet", document.getElementById("gameInput").value); }
+function winner(side){ socket.emit("setWinner", side); }
+function clearWinner(){ socket.emit("clearWinner"); }
 
 function timerStart(){ socket.emit("timerStart"); }
 function timerStop(){ socket.emit("timerStop"); }
